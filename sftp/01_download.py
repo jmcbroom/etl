424226c79869c8430a3bd2ed5b04ef447cbe14fc
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-05-23 16:07:19 -0400

    finish data cleaning, add config file

--- sftp/01_download.py
+++ sftp/01_download.py
@@ -25,5 +25,29 @@ with pysftp.Connection(os.environ['SFTP_HOST'], port=2222, username=os.environ['
 # create a dataframe from the local copy
 df = pandas.read_csv(file_path)
 
+def clean_cols(name):
+	"""Format column names"""
+	name = name.replace(":", "")
+	name = name.replace(" ", "_").lower().strip()
+	return name
+
+# rename cols
+df.rename(columns=lambda x: clean_cols(x), inplace=True)
+
+def make_float(value):
+	"""Format money strings as number"""
+	value = value.replace("$", "")
+	return float(value)
+
+# format money col
+df['contract_value'] = df['contract_value'].apply(lambda x: make_float(x))
+
+# format date cols as actual datetimes
+df['contract_effective_date'] = pandas.to_datetime(df['contract_effective_date'])
+df['contract_contract_expiration_date'] = pandas.to_datetime(df['contract_contract_expiration_date'])
+
+# confirm col datetypes are good
+print(df.dtypes)
+
 # write the dataframe to a central db
-odo.odo(df, 'postgresql:://gisteam@localhost/etl::ocp')
+odo.odo(df, 'postgresql://gisteam@localhost/etl::ocp')
