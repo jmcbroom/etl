608aab11735e6acdb81c7abae04217f93a17e4cd
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-08-09 08:39:15 -0400

    add scripts for daily view of rms

---
+++ process/dpd/rms_daily/update.sh
@@ -0,0 +1,8 @@
+echo "Beginning RMS (Private View) update..."
+python $FILEROOT/01_get_update.py
+echo "Anonymizing new locations..."
+psql -d $PG_DB < $FILEROOT/02_anonymize_updates.sql
+echo "Inserting new data into main table..."
+psql -d $PG_DB -c "insert into rms (select * from rms_daily_update)"
+echo "Dropping temporary table..."
+psql -d $PG_DB -c "drop table if exists rms_daily_update"
