ca3dbf01f5fbf32304d9f659492f7dbc60487c9d
Jimmy McBroom <james.mcbroom@gmail.com>
2017-08-11 15:59:38 -0400

    more dataframing

--- etl/salesforce.py
+++ etl/salesforce.py
@@ -11,19 +11,26 @@ wheredict = {
 }
 
 def clean_sf_col(colname):
-    # if colname.endswith('__c'):
-        # colname = colname[:-3]
-    return colname.split('.')[-1]
+    split = colname.split('.')
+    if colname.endswith("RecordType"):
+        return "RecordType.DeveloperName"
+    # handle child relationships
+    if len(split) == 3:
+        split[1] = split[1].replace("__c", "__r")
+    return ".".join(split[1:])
 
 class SfReport(object):
-    def __init__(self, report_id='00Of1000004PL1oEAG'):
-        params = { 'includeDetails': 'false' }
+    def __init__(self, report_id='00Of1000004PL1oEAG', include_details=False):
+        if include_details:
+            params = { 'includeDetails': 'true' }
+        else:
+            params = { 'includeDetails': 'false' }
         self.report = SF.restful("analytics/reports/{}".format(report_id), params=params)
         self.lookup = { clean_sf_col(k): v['label'] for k, v in self.report['reportExtendedMetadata']['detailColumnInfo'].items() }
-        self.wheres = [ "{} {} {}".format(f['column'], wheredict[f['operator']], f['value']) for f in self.report['reportMetadata']['reportFilters'] ]
-    
+        self.wheres = [ "{} {} {}".format(clean_sf_col(f['column']), wheredict[f['operator']], f['value']) for f in self.report['reportMetadata']['reportFilters'] ]
+
     def to_dataframe(self):
-        pass
+        return "Select {} from Case where {}".format(",".join(self.lookup.keys()), " and ".join(self.wheres))
         
 if __name__ == "__main__":
     fire.Fire(SfReport)
