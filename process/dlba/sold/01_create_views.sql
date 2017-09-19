72af5a7d048b78e61249af7d16452cfd0d584c8e
Jimmy McBroom <james.mcbroom@gmail.com>
2017-09-19 11:28:56 -0400

    create table statements

--- process/dlba/sold/01_create_views.sql
+++ process/dlba/sold/01_create_views.sql
@@ -39,6 +39,28 @@ inner join dlba.case c on a.case = c.id
 inner join dlba.prospective_buyer pb on pb.dlba_activity = a.id
 where a.dlba_activity_type in ('Demo Pull Sale','Demo Pull for Demo Sale','Own It Now','Own It Now - Bundled Property')
 and a.actual_closing_date is not null
-and pb.buyer_status = 'Selected');
+and pb.buyer_status = 'Selected'
+);
 
--- all sold
+-- auction sold
+create table dlba.auction_sold as (
+select 
+a.actual_closing_date,
+a.sale_status, 
+c.address, 
+c.parcel_id,
+'Auction' as program, 
+c.neighborhood, 
+c.council_district, 
+c.acct_latitude, 
+c.acct_longitude, 
+pb.buyer_status,
+pb.final_sale_price,
+pb.purchaser_type
+from dlba.dlba_activity a
+inner join dlba.case c on a.case = c.id
+inner join dlba.prospective_buyer pb on pb.dlba_activity = a.id
+where a.recordtypeid = '012j0000000xtGoAAI' 
+and a.sale_status = 'Closed'
+and pb.buyer_status = 'Selected'
+);
