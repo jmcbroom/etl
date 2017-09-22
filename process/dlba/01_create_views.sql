d41e79736587c964bf7f593a614e9b0b0089e290
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-09-22 10:27:06 -0400

    add for sale query and config

--- process/dlba/01_create_views.sql
+++ process/dlba/01_create_views.sql
@@ -1,4 +1,4 @@
--- side lots
+-- side lots sold
 create table dlba.side_lots as (
 select 
 a.actual_closing_date,
@@ -22,7 +22,7 @@ and pb.buyer_status = 'Selected'
 and c.address not like 'Fake%'
 );
 
--- own it now
+-- own it now sold
 create table dlba.own_it_now as (
 select 
 a.actual_closing_date,
@@ -67,5 +67,26 @@ inner join dlba.prospective_buyer pb on pb.dlba_activity = a.id
 where a.recordtypeid = '012j0000000xtGoAAI' 
 and a.sale_status = 'Closed'
 and pb.buyer_status = 'Selected'
+and c.address not like 'Fake%'
+);
+
+-- for sale
+create table dlba.for_sale as (
+select
+c.address,
+c.parcel_id,
+c.program,
+a.listing_date
+c.neighborhood,
+c.council_district,
+c.acct_latitude,
+c.acct_longitude
+from dlba.dlba_activity a
+inner join dlba.case c on a.case = c.id
+where (a.recordtypeid = '012j0000000xtGoAAI'
+or a.dlba_activity_type in ('Demo Pull Sale', 'Demo Pull for Demo Sale', 'Renovation Sale', 'Own It Now', 'Own It Now - Bundled Property', 'Auction - Bundled Property'))
+and a.listing_date < now()
+and a.sale_status = 'For Sale On Site'
+and c.status = 'For Sale'
 and c.address not like 'Fake%'
 );
