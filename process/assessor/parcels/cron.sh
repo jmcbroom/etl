e2ddd2a9e333980f686884e39d774c88b1669d31
Jimmy McBroom <james.mcbroom@gmail.com>
2017-10-04 14:35:27 -0400

    remove parcel cruft

--- process/assessor/parcels/cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/gisteam/.env
-/home/gisteam/anaconda3/bin/python /home/gisteam/etl/tools/socrata.py update --dir="assessor/parcels"
-/home/gisteam/anaconda3/bin/python /home/gisteam/etl/tools/socrata.py create_db_view --dir="assessor/parcels"
-/home/gisteam/anaconda3/bin/python /home/gisteam/etl/tools/socrata.py delete_all_rows --dir="assessor/parcels"
-/home/gisteam/anaconda3/bin/python /home/gisteam/etl/tools/socrata.py upsert --dir="assessor/parcels"
--- process/assessor/parcels/update.sh
+++
@@ -1,5 +0,0 @@
-echo $FILEROOT
-echo "Beginning parcels update..."
-/home/gisteam/anaconda3/bin/python $FILEROOT/01_download.py
-echo "Joining to attrib table"
-psql -d $PG_DB < $FILEROOT/02_join.sql
