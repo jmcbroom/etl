e83ab2bc082975e0c436e5cd3e747ccf359ecf0e
jessica <jessica.mcinchak@gmail.com>
2017-10-23 16:12:29 -0400

    stubbing out a database class and bld permits

---
+++ etl/database.py
@@ -0,0 +1,35 @@
+import sqlalchemy
+import cx_Oracle
+import pandas
+import odo
+from os import environ as env
+
+def get_table_as_df(dbType, dbTable, connString):
+  if db_type == 'oracle':
+    engine = sqlalchemy.create_engine('{}'.format(connString))
+    conn = engine.connect()
+    res = conn.execute('select * from {}'.format(table))
+    df = pandas.DataFrame(res.fetchall())
+    df.columns = res.keys
+  elif db_type == 'sqlserver':
+    pass
+
+  return df
+
+def make_db_connection_string(dbType, envPrefix):
+  connection_string = dbType + '://'
+  # then concat prefix with '_USER' etc to mock env variable format to be formatted into template literal?
+
+  return connection_string
+
+class Database(object):
+  def __init__(self, db_type, table, prefix):
+    self.db_type = db_type
+    self.table = table
+    self.prefix = prefix
+
+  def to_postgres(self):
+  	self.conn = make_db_connection_string(self.db_type, self.prefix)
+  	self.df = get_table_as_df(self.db_type, self.table, self.conn)
+
+  	odo.odo(df, "postgres://{}@localhost/{}::{}".format(env['PG_USER'], env['PG_DB'], self.table))
