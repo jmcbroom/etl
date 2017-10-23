b338c2133b7f1be3527405916eafcc40d8382988
Jimmy McBroom <james.mcbroom@gmail.com>
2017-10-23 15:43:47 -0400

    basic Salesforce extract; turn 01_extract.yml into an array of sources

---
+++ etl/salesforce.py
@@ -0,0 +1,31 @@
+import simple_salesforce
+import pandas
+import odo
+import sqlalchemy
+
+from os import environ as env
+
+SF = simple_salesforce.Salesforce(env['SF_USER'], env['SF_PASS'], env['SF_TOKEN'])
+
+local_engine = sqlalchemy.create_engine("postgresql://{}@localhost/{}".format(env['PG_USER'], env['PG_DB']))
+local_connection = local_engine.connect()
+
+class SfTable(object):
+  """Class representing a Salesforce table."""
+
+  def __init__(self, params):
+    self.object = params['object']
+    self.fields = params['fields']
+    self.schema = params['schema']
+    self.query = "Select {} from {}".format(",".join(self.fields), self.object)
+    print(self.query)
+
+  def to_postgres(self):
+    res = SF.query_all(self.query)
+    df = pandas.DataFrame.from_records(res['records'])
+    df.columns = df.columns.str.lower().str.replace('__c', '')
+    df.drop('attributes', inplace=True, axis=1)
+
+    local_connection.execute("drop table if exists {}.{}".format(self.schema, self.object.lower()))
+    
+    odo.odo(df, "postgresql://{}@localhost/{}::{}".format(env['PG_USER'], env['PG_DB'], self.object.lower()), schema=self.schema)
