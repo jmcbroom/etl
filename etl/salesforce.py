d2777821cffe56f3abdbdd17041facb7fa8a1933
Jimmy McBroom <james.mcbroom@gmail.com>
2017-11-04 09:58:50 -0400

    first pass at flattening out nested OrderedDict

--- etl/salesforce.py
+++ etl/salesforce.py
@@ -10,6 +10,31 @@ SF = simple_salesforce.Salesforce(env['SF_USER'], env['SF_PASS'], env['SF_TOKEN'
 local_engine = sqlalchemy.create_engine("postgresql://{}/{}".format(env['PG_CONNSTR'], env['PG_DB']))
 local_connection = local_engine.connect()
 
+def flatten_record(rec):
+    """Take a record & flatten out any OrderedDicts"""
+    new_rec = {}
+    for k,v in rec.items():
+        # don't copy over 'attributes'
+        if k == 'attributes':
+            pass
+        # if it's a relationship...
+        elif k.endswith('__r'):
+            # iterate through the OrderedDict we're given
+            for a, b in v.items():
+                # ignore these keys
+                if a not in ['type', 'url', 'attributes']:
+                    # construct a new key from the top-level key and the field name
+                    key = k.lower().rstrip('__r') + '_' + a.lower().rstrip('__c')
+                    # create it
+                    new_rec[key] = b
+        # strip off '__c'
+        elif k.endswith('__c'):
+            new_rec[k.lower().rstrip('__c')] = v
+        # no __c? just add it
+        else:
+            new_rec[k.lower()] = v
+    return new_rec
+
 class SfTable(object):
   """Class representing a Salesforce table."""
 
