b446ae23b00d0cc312940d43ae0143b67b7498a1
Jimmy McBroom <james.mcbroom@gmail.com>
2017-11-06 10:40:19 -0500

    process record

--- etl/salesforce.py
+++ etl/salesforce.py
@@ -11,7 +11,6 @@ local_engine = sqlalchemy.create_engine("postgresql://{}/{}".format(env['PG_CONN
 local_connection = local_engine.connect()
 
 def flatten_record(rec):
-    """Take a record & flatten out any OrderedDicts"""
     new_rec = {}
     for k,v in rec.items():
         # don't copy over 'attributes'
@@ -19,20 +18,24 @@ def flatten_record(rec):
             pass
         # if it's a relationship...
         elif k.endswith('__r'):
-            # iterate through the OrderedDict we're given
-            for a, b in v.items():
-                # ignore these keys
-                if a not in ['type', 'url', 'attributes']:
-                    # construct a new key from the top-level key and the field name
-                    key = k.lower().rstrip('__r') + '_' + a.lower().rstrip('__c')
-                    # create it
-                    new_rec[key] = b
+            print(v)
+            try:
+                # iterate through the OrderedDict we're given
+                for a, b in v.items():
+                    # ignore these keys
+                    if a not in ['type', 'url', 'attributes']:
+                        # construct a new key from the top-level key and the field name
+                        key = k.rstrip('__r') + '_' + a.rstrip('__c')
+                        # create it
+                        new_rec[key] = b
+            except AttributeError:
+                pass
         # strip off '__c'
         elif k.endswith('__c'):
-            new_rec[k.lower().rstrip('__c')] = v
+            new_rec[k.rstrip('__c')] = v
         # no __c? just add it
         else:
-            new_rec[k.lower()] = v
+            new_rec[k] = v
     return new_rec
 
 class SfTable(object):
@@ -47,9 +50,9 @@ class SfTable(object):
 
   def to_postgres(self):
     res = SF.query_all(self.query)
-    df = pandas.DataFrame.from_records(res['records'])
-    df.columns = df.columns.str.lower().str.replace('__c', '')
-    df.drop('attributes', inplace=True, axis=1)
+    df = pandas.DataFrame.from_records([ flatten_record(r) for r in res['records'] ])
+    df.columns = df.columns.str.lower()
+    # df.drop('attributes', inplace=True, axis=1)
 
     local_connection.execute("drop table if exists {}.{}".format(self.schema, self.object.lower()))
     
