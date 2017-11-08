0c603e50a517e39196dbaf608086c77ba51076bf
Jimmy McBroom <james.mcbroom@gmail.com>
2017-11-08 13:36:06 -0500

    works now

--- schedules.py
+++ schedules.py
@@ -6,8 +6,8 @@ def run(process):
   p = etl.Process(process)
   p.update()
 
-schedule.every(5).minutes.do(run('angels_night'))
-schedule.every.tuesday.do(run('medical_marijuana'))
+schedule.every(1).minutes.do(run, process='angels_night')
+# schedule.every.tuesday.do(run('medical_marijuana'))
 
 while True:
     schedule.run_pending()
