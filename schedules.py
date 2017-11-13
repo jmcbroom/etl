3e87a30b2c736050dd722b91565e9c360900d65f
Jimmy McBroom <james.mcbroom@gmail.com>
2017-11-13 15:45:31 -0500

    daily thread; try/except on run(process) so we dont crash

--- schedules.py
+++ schedules.py
@@ -2,11 +2,20 @@ import schedule
 import time
 import etl
 
+from etl.slack import SlackMessage
+
+msg = SlackMessage({"text": "ETL thread for 2017/11/13"})
+msg.send()
+
 def run(process):
   p = etl.Process(process)
-  p.update()
+  try:
+    p.update()
+  except Error:
+    msg.comment("Error on {}".format(process))
 
-schedule.every(1).minutes.do(run, process='angels_night')
+
+schedule.every(5).minutes.do(run, process='angels_night')
 # schedule.every.tuesday.do(run('medical_marijuana'))
 
 while True:
