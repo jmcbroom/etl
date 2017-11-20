2aa0e0e4c5900452ec96d805eccd0d324743ca8f
Jessica McInchak <jessica.mcinchak@gmail.com>
2017-11-20 13:20:06 -0500

    refactor lookup funcs

--- etl/lookup.py
+++ etl/lookup.py
@@ -1,42 +1,87 @@
 import pandas
 import re
-import csv
 import sqlalchemy
 from os import environ as env
 from .utils import connect_to_pg, df_to_pg
 
+conn = connect_to_pg()
+
 def read_file(path):
     """ read an external csv into new postgres table """
     df = pandas.read_csv("{}/{}".format(env['FILEROOT'], path))
-    conn = connect_to_pg()
     conn.execute("drop table if exists pubsafe.misfits cascade;")
     df_to_pg(df, 'pubsafe', 'misfits')
 
-def create_flag():
-    """ create new column to indicate if row has been anonymized yet, set to false by default """
-    conn = connect_to_pg()
-    conn.execute("alter table {} add column is_anon boolean".format(self.table))
-    conn.execute("update {} set is_anon = {}".format(self.table, self.set_flag))
+def redact_location(loc):
+    """ redact incident address up front if contains key words or starts with @ """
+    redactors = [
+        'ARMED', 
+        'ASSAULT', 
+        'ASSIST', 
+        'AUTO', 
+        'CARJACKING',
+        'DEATH', 
+        'DESTRUCTION', 
+        'FATAL', 
+        'FELONIOUS', 
+        'INVESTIGATION', 
+        'KIDNAPPING', 
+        'LARCENY', 
+        'LOOK OUT', 
+        'MISSING', 
+        'NARCOTICS',
+        'REMARKS',
+        'REPORT',
+        'ROBBERY',
+        'SERIOUS',
+        'SHOOTING',
+        'SHOT',
+        'SPEC ATTEN',
+        'SPEC ATTN',
+        'SPECIAL ATTENTION',
+        'STABBED',
+        'UNIT',
+        'WEAPON',
+        'chasing']
 
-def lookup_address():
-    """ if incident address is place with known address, replace with street address before anonymization, set_flag remains false """
-    conn = connect_to_pg()
-    query = "update pubsafe.cad_update c set incident_address = geoc_address from pubsafe.misfits m where c.incident_address = m.unique_misfit"
-    conn.execute(query)
+    redactor = re.findall(r"(?=("+'|'.join(redactors)+r"))", loc)
+    starter = "@"
 
-def accept_place():
-    """ accept common place names for incident address (eg 'Woodward Ave'), set_flag is true """
-    conn = connect_to_pg()
-    query = "update pubsafe.cad_update c set incident_address = anon_return, is_anon = 'true' from pubsafe.misfits m where c.incident_address = m.unique_misfit"
-    conn.execute(query)
+    if len(redactor) > 0 or loc.startswith(starter):
+        loc = "Location Redacted"
+    else:
+        pass
 
-def redact_location():
-    """ redact incident address up front if contains key words or starts with @, set_flag is true """
+    return loc
 
 class LookupValues(object):
-  def __init__(self, table='cad_update', field='incident_address', file='misfits_lookup.csv', on='match', set_flag='false'):
-    self.table = table
-    self.field = field
-    self.file = file
-    self.on = on
-    self.set_flag = set_flag
+    def __init__(self, table='pubsafe.cad_update', field='incident_address', file='misfits_lookup.csv', on='match', set_flag='false'):
+        self.table = table
+        self.field = field
+        self.file = file
+        self.on = on
+        self.set_flag = set_flag
+
+    def lookup(self):
+        # find known street addresses for incident places, is anonymized remains false
+        addr_lookup = """update {} c 
+                        set {} = geoc_address 
+                        from pubsafe.misfits m 
+                        where c.{} = m.unique_misfit
+                        and m.geoc_address is not null""".format(self.table, self.field, self.field)
+        
+        # accept common names for incident places, is anonymized is true
+        place_lookup = """update {} c 
+                        set {} = anon_return, is_anon = 'true' 
+                        from pubsafe.misfits m 
+                        where c.{} = m.unique_misfit
+                        and m.anon_return is not null""".format(self.table, self.field, self.field)
+        
+        # redact bad words before anonymizing, is anonymized is true
+        redaction_lookup = """update {}
+                            set {} = {}, is_anon = 'true'
+                            where {} = {}""".format(self.table, self.field, redact_location(v), self.field, v)
+
+        for q in [addr_lookup, place_lookup, redact_location]:
+            print(q)
+            conn.execute(q)
