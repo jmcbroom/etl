55863eef63bb7eebdb76654b3f95b968ab8b3d3c
Jessica McInchak <jessica.mcinchak@gmail.com>
2017-12-21 10:36:29 -0500

    add schedule for hourly updates

---
+++ schedules_hrly.py
@@ -0,0 +1,23 @@
+import time
+import etl
+import arrow
+
+from etl.slack import SlackMessage
+
+this_day = arrow.now().format('dddd, MMMM Do')
+msg = SlackMessage({"text": "ETL hourly updates for {}".format(this_day)})
+msg.send()
+
+def run(process, dataset=None, notify=False, emoji=None):
+  try:
+    p = etl.Process(process)
+    p.update(dataset)
+    if notify:
+      msg.comment("Update successful: *{}*".format(process))
+      msg.react(emoji)
+  except Exception as e:
+    msg.comment("Error: *{}*\n > `{}`".format(process, e))
+
+# # daily run; these do not use schedule
+run('crimescape', dataset=None, notify=True, emoji='rotating_light')
+run('angels_night', dataset=None, notify=True, emoji='fire')
