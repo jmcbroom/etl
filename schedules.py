713a1680edb326e1564f255fcd3e9601e6434c47
Jimmy McBroom <james.mcbroom@gmail.com>
2018-02-08 10:06:48 -0500

    new processes from dev

--- schedules.py
+++ schedules.py
@@ -1,32 +1,35 @@
 import time
 import etl
 import arrow
+import socket
 
 from etl.slack import SlackMessage
 
 this_day = arrow.now().format('dddd, MMMM Do')
-msg = SlackMessage({"text": "ETL thread for {}".format(this_day)})
+msg = SlackMessage({"text": "({}) ETL thread for {}".format(socket.gethostname(), this_day)})
 msg.send()
 
 def run(process, dataset=None, notify=False, emoji=None):
   try:
     p = etl.Process(process)
-    p.update(dataset)
+    p.extract()
+    p.transform()
     if notify:
       msg.comment("Update successful: *{}*".format(process))
       msg.react(emoji)
   except Exception as e:
-    msg.comment("Error: *{}*\n > `{}`".format(process, e))
+    pass
+    # msg.comment("Error: *{}*\n > `{}`".format(process, e))
 
 # # daily run; these do not use schedule
-run('dlba', dataset=None, notify=True, emoji='bank')
-run('assessor', dataset=None, notify=True, emoji='ledger')
-run('medical_marijuana', dataset=None, notify=True, emoji='herb')
-run('cad', dataset=None, notify=True, emoji='ambulance')
-run('rms', dataset=None, notify=True, emoji='police_car')
-run('bseed', dataset=None, notify=True, emoji='nut_and_bolt')
-run('ocp', dataset=None, notify=True, emoji='money_with_wings')
-run('blight_violations', dataset=None, notify=True, emoji='warning')
+run('dlba', dataset=None, notify=False, emoji='bank')
+run('assessor', dataset=None, notify=False, emoji='ledger')
+run('medical_marijuana', dataset=None, notify=False, emoji='herb')
+run('cad', dataset=None, notify=False, emoji='ambulance')
+run('rms', dataset=None, notify=False, emoji='police_car')
+run('bseed', dataset=None, notify=False, emoji='nut_and_bolt')
+run('ocp', dataset=None, notify=False, emoji='money_with_wings')
+run('blight_violations', dataset=None, notify=False, emoji='warning')
 
 ## Scheduling datasets
 # Angel's Night fire data
