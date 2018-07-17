559ecf86261588266f5463d387c3db8c61d011f2
Jimmy McBroom <james.mcbroom@gmail.com>
2018-07-17 11:20:17 -0400

    new Dataset class; inherits t,l from Process

--- etl/process.py
+++ etl/process.py
@@ -4,23 +4,100 @@ from .utils import connect_to_pg, add_geom_column, exec_psql_query, drop_table_i
 DATA_DIR = "{}/process".format(os.environ['ETL_ROOT'])
 connection = connect_to_pg()
 
+class Dataset(object):
+  def __init__(self, name, basedir):
+    self.name = name
+    self.basedir = basedir
+    self.refresh()
+
+  def refresh(self):
+    with open("{}/metadata/{}.yml".format(self.basedir, self.name), 'r') as f:
+      self.m = yaml.load(f)
+    with open("{}/transform/{}.yml".format(self.basedir, self.name), 'r') as f:
+      self.t = yaml.load(f)
+    with open("{}/load/{}.yml".format(self.basedir, self.name), 'r') as f:
+      self.l = yaml.load(f)
+
+  def transform(self):
+    for s in self.t:
+      if s['type'] == 'geocode':
+        from etl.geocode import GeocodeTable
+        # add geometry column and index, specified in YML
+        add_geom_column(connection, s['table'], s['geom_col'])
+        # batch geocode addresses
+        if 'parcel_col' in s.keys():
+          GeocodeTable(s['table'], s['add_col'], s['geom_col'], s['parcel_col']).geocode_rows()
+        elif 'where_clause' in s.keys() and 'parcel_col' in s.keys():
+          GeocodeTable(s['table'], s['add_col'], s['geom_col'], s['parcel_col'], s['where_clause']).geocode_rows()
+        elif 'where_clause' in s.keys() and 'parcel_col' not in s.keys():
+          GeocodeTable(s['table'], s['add_col'], s['geom_col'], None, s['where_clause']).geocode_rows()
+        else:
+          GeocodeTable(s['table'], s['add_col'], s['geom_col']).geocode_rows()
+
+      if s['type'] == 'sql':
+        for statement in s['statements']:
+          exec_psql_query(connection, statement, verbose=True)
+
+      if s['type'] == 'anonymize_text_location':
+        from etl.anonymize import AnonTextLocation
+        atl = AnonTextLocation(s['table'], s['column'], s['set_flag'])
+        atl.anonymize()
+
+      if s['type'] == 'anonymize_geometry':
+        from etl.anonymize import AnonGeometry
+        ag = AnonGeometry(s['table'], s['against'])
+        ag.anonymize()
+
+      if s['type'] == 'lookup':
+        print(s)
+        from etl.lookup import LookupValues
+        look = LookupValues(s['table'], s['lookup_field'], s['file'], s['match_field'], s['method'], s['set_flag'])
+        look.lookup()
+  
+  def load(self, dataset=None):
+    for d in self.l:
+
+      # if we specified a name, filter based on that
+      if dataset == None or (dataset and d['name'] == dataset):
+
+        # switch on destination
+        destination = d.pop('to', None)
+        if destination == 'Socrata':
+          from .socrata import Socrata
+          s = Socrata(d)
+          s.update()
+
+        elif destination == 'ArcGIS Online':
+          from .arcgis import AgoLayer
+          l = AgoLayer(d)
+          l.publish()
+
+        elif destination == 'SFTP':
+          from .sftp import Sftp
+          s = Sftp(d['host'], None, d['file'])
+
+        elif destination == 'Mapbox':
+          from .mapbox import MapboxUpload
+          m = MapboxUpload(d)
+          m.upload()
+
+        else:
+          print("I don't know this destination type: {}".format(destination))
+
 class Process(object):
   def __init__(self, directory="project_greenlight"):
     self.basedir = "{}/{}".format(DATA_DIR, directory)
     self.refresh()
     self.process_name = self.m['name']
     self.schema = self.m['schema']
+    self.datasets = self.m['datasets']
 
   def refresh(self):
     with open("{}/00_metadata.yml".format(self.basedir), 'r') as f:
       self.m = yaml.load(f)
     with open("{}/01_extract.yml".format(self.basedir), 'r') as f:
       self.e = yaml.load(f)
-    with open("{}/02_transform.yml".format(self.basedir), 'r') as f:
-      self.t = yaml.load(f)
-    with open("{}/03_load.yml".format(self.basedir), 'r') as f:
-      self.l = yaml.load(f)
-  
+
   def extract(self):
     for source in self.e:
       for srctype, params in source.items():
@@ -90,76 +167,10 @@ class Process(object):
         else:
           print("I don't know this source type: {}".format(srctype))
 
-  def transform(self):
-    for s in self.t:
-      if s['type'] == 'geocode':
-        from etl.geocode import GeocodeTable
-        # add geometry column and index, specified in YML
-        add_geom_column(connection, s['table'], s['geom_col'])
-        # batch geocode addresses
-        if 'parcel_col' in s.keys():
-          GeocodeTable(s['table'], s['add_col'], s['geom_col'], s['parcel_col']).geocode_rows()
-        elif 'where_clause' in s.keys() and 'parcel_col' in s.keys():
-          GeocodeTable(s['table'], s['add_col'], s['geom_col'], s['parcel_col'], s['where_clause']).geocode_rows()
-        elif 'where_clause' in s.keys() and 'parcel_col' not in s.keys():
-          GeocodeTable(s['table'], s['add_col'], s['geom_col'], None, s['where_clause']).geocode_rows()
-        else:
-          GeocodeTable(s['table'], s['add_col'], s['geom_col']).geocode_rows()
-
-      if s['type'] == 'sql':
-        for statement in s['statements']:
-          exec_psql_query(connection, statement, verbose=True)
-
-      if s['type'] == 'anonymize_text_location':
-        from etl.anonymize import AnonTextLocation
-        atl = AnonTextLocation(s['table'], s['column'], s['set_flag'])
-        atl.anonymize()
-
-      if s['type'] == 'anonymize_geometry':
-        from etl.anonymize import AnonGeometry
-        ag = AnonGeometry(s['table'], s['against'])
-        ag.anonymize()
-
-      if s['type'] == 'lookup':
-        print(s)
-        from etl.lookup import LookupValues
-        look = LookupValues(s['table'], s['lookup_field'], s['file'], s['match_field'], s['method'], s['set_flag'])
-        look.lookup()
-
-  def load(self, dataset=None):
-    for d in self.l:
-
-      # if we specified a name, filter based on that
-      if dataset == None or (dataset and d['name'] == dataset):
-
-        # switch on destination
-        destination = d.pop('to', None)
-        if destination == 'Socrata':
-          from .socrata import Socrata
-          s = Socrata(d)
-          s.update()
-
-        elif destination == 'ArcGIS Online':
-          from .arcgis import AgoLayer
-          l = AgoLayer(d)
-          l.publish()
-
-        elif destination == 'SFTP':
-          from .sftp import Sftp
-          s = Sftp(d['host'], None, d['file'])
-
-        elif destination == 'Mapbox':
-          from .mapbox import MapboxUpload
-          m = MapboxUpload(d)
-          m.upload()
-
-        else:
-          print("I don't know this destination type: {}".format(destination))
-
   def update(self, dataset=None):
     self.extract()
+    for d in self.datasets:
+      ds = Dataset(d, self.basedir)
+      ds.transform()
+      ds.load()
     self.transform()
-    if dataset:
-      self.load(dataset=dataset)
-    else:
-      self.load()
