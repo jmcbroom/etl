4212eadcef7aa6eb249522d226a33a2ab0b4db53
Jimmy McBroom <james.mcbroom@gmail.com>
2018-10-01 08:30:35 -0400

    rest inspections extract work

--- etl/flatfile.py
+++ etl/flatfile.py
@@ -1,4 +1,4 @@
-from .utils import df_to_pg
+from .utils import df_to_pg, clean_column
 import pandas
 
 # Make a pandas dataframe from a flatfile that we can send to postgres
@@ -7,16 +7,20 @@ class Flatfile(object):
         self.config = params
 
     def to_postgres(self):
-        if self.config['source'] == 'swordsolutions':
-            path = '/home/gisteam/ExpData'
-            delimiter = '/t'
-            lineterminator = '/n'
-            encoding = 'ISO-8859-1'
+        if self.config['sys'] == 'swordsolutions':
+            path = "/home/gisteam/ExpData"
+            delimiter = "/t"
+            lineterminator = "/n"
+            encoding = "ISO-8859-1"
+            
+            filename = '{}/{}'.format(path, self.config['name'])
+            df = pandas.read_csv(filename, sep="\t", lineterminator="\n", encoding="ISO-8859-1")
+            df.rename(columns=lambda x: clean_column(x), inplace=True)
+            print(df.columns)
+            df = df.where((pandas.notnull(df)), None)
+            schema, table = self.config['destination'].split('.')
+            df_to_pg(df, schema, table)
         else:
             pass
 
-      filename = '{}/{}'.format(path, self.config['name'])
-      df = pandas.read_csv(filename, sep=delimiter, lineterminator=lineterminator, encoding=encoding)
-      df = df.where((pandas.notnull(df)), None)
-      schema, table = self.config['destination'].split('.')
-      df_to_pg(df, schema, table)
+
